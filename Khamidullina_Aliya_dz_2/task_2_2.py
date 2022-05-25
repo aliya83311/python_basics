5321f484fe07c23dc7aed83d20c13dddcaf109a1
Aliya83311 <hamidullina833@gmail.com>
2022-05-26 02:32:51 +0700

    Added task 2

--- Khamidullina_Aliya_dz_2/task_2_2.py
+++ Khamidullina_Aliya_dz_2/task_2_2.py
@@ -5,13 +5,12 @@ def add_zero(seq):
     for item in range(len(seq)):
         if seq[item].startswith('+'):
             seq[item] = seq[item].replace('+', '0')
-    for item in range(len(seq)):
         if seq[item].isdigit():
             seq[item] = seq[item].zfill(2)
-            seq.insert(item + 1, '"')
-            seq.insert(item - 1, '"')
+    seq = ['"' + item + '"' if item.isdigit() else item for item in seq]
     return ' '.join(seq)
 
 
+print(id(weather))
 print(add_zero(weather))
-
+print(id(weather))
