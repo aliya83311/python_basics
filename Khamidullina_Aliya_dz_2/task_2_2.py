b78a0a308d8af2a9f762a9d6a12d98555cb0e066
Aliya83311 <hamidullina833@gmail.com>
2022-05-26 23:10:34 +0700

    Fixed mistakes

--- Khamidullina_Aliya_dz_2/task_2_2.py
+++ Khamidullina_Aliya_dz_2/task_2_2.py
@@ -4,13 +4,12 @@ weather = ['в', '5', 'часов', '17', 'минут', 'температура'
 def add_zero(seq):
     for item in range(len(seq)):
         if seq[item].startswith('+'):
-            seq[item] = seq[item].replace('+', '0')
-            seq[item] = f'"+{seq[item]}"'
+            seq[item] = f'+{seq[item].replace("+", "0")}'
+            seq.insert(item, '"')
+#            seq.insert(item + 1, '"')
         if seq[item].isdigit():
-            seq[item] = f'"{seq[item].zfill(2)}"'
+            seq[item] = '"' + seq[item].zfill(2) + '"'
     return ' '.join(seq)
 
 
-print(id(weather))
 print(add_zero(weather))
-print(id(weather))
--- Khamidullina_Aliya_dz_2/task_2_5.py
+++ Khamidullina_Aliya_dz_2/task_2_5.py
@@ -1,28 +1,28 @@
-prices = [57.8, 46.51, 97, 9.23, 100.4, 34, 5, 2356.04]
-
-
-def human_readable_price(price):
-    if isinstance(price, int):
-        return f"{str(price).zfill(2)} руб. 00 коп."
-    else:
-        cents = str(price).split(".")[1]
-        roubles = int(price)
-        return f"{str(roubles).zfill(2)} руб. {cents.zfill(2)} коп."
+prices = [57.8, 46.51, 97, 9.23, 100.4, 34, 5, 2356.04, 10999.99, 0.99]
 
 
 def get_rub(x):
     return int(x[:x.index(" ")])
 
 
-print(f"Id списка prices {id(prices)}")
-new_prices = list(map(human_readable_price, prices))
-print(new_prices)
-print(f"Id списка new_prices {id(new_prices)}")
+def human_readable_price(seq):
+    for idx, price in enumerate(seq):
+        if isinstance(seq[idx], int):
+            seq[idx] = f"{str(price)} руб 00 коп"
+        else:
+            cents = str(price).split(".")[1]
+            roubles = int(price)
+            seq[idx] = f"{str(roubles)} руб {cents.zfill(2)} коп"
+    return seq
 
-new_prices.sort(key=get_rub)
-print(new_prices)
-print(f"Id списка new_prices {id(new_prices)}")
 
-prices_descending = sorted(new_prices, key=get_rub, reverse=True)
+print(f"ID списка 'prices': {id(prices)}")
+print(human_readable_price(prices))
+
+prices.sort(key=get_rub)
+print(prices)
+print(f"ID списка 'prices': {id(prices)}")
+
+prices_descending = sorted(prices, key=get_rub, reverse=True)
 print(prices_descending)
 print(prices_descending[:5])
