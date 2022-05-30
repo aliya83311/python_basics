7564b314e7330ba80fb11e5e75288700b36952cb
Aliya83311 <hamidullina833@gmail.com>
2022-05-30 21:53:02 +0700

    Added an argument "repeat"

--- Khamidullina_Aliya_dz_3/task_3_5.py
+++ Khamidullina_Aliya_dz_3/task_3_5.py
@@ -5,19 +5,39 @@ adverbs = ["сегодня", "вчера", "завтра", "позавчера",
 adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
 
 
-def get_jokes(num):
+def get_jokes(num, repeat=True):
     """ Make jokes from lists.
 
     Arguments:
     num -- number of jokes the user wants to make.
+    repeat -- defines if one word can be used in multiple jokes.
+    Returns a list containing strings.
     """
     result = []
-    for n in range(num):
-        noun = nouns[randrange(len(nouns))]
-        adverb = adverbs[randrange(len(adverbs))]
-        adjective = adjectives[randrange(len(adjectives))]
-        result.append(f"{noun} {adverb} {adjective}")
-    return result
+    if repeat:
+        for n in range(num):
+            noun = nouns[randrange(len(nouns))]
+            adverb = adverbs[randrange(len(adverbs))]
+            adjective = adjectives[randrange(len(adjectives))]
+            result.append(f"{noun} {adverb} {adjective}")
+        return result
+    else:
+        nouns1 = nouns.copy()
+        adverbs1 = adverbs.copy()
+        adjectives1 = adjectives.copy()
+        try:
+            for n in range(num):
+                noun = nouns1[randrange(len(nouns1))]
+                adverb = adverbs1[randrange(len(adverbs1))]
+                adjective = adjectives1[randrange(len(adjectives1))]
+                result.append(f"{noun} {adverb} {adjective}")
+                nouns1.pop(nouns1.index(noun))
+                adverbs1.pop(adverbs1.index(adverb))
+                adjectives1.pop(adjectives1.index(adjective))
+        except ValueError:
+            print("Закончились слова в списках!")
+        finally:
+            return result
 
 
-print(get_jokes(3))
+print(get_jokes(8, False))
