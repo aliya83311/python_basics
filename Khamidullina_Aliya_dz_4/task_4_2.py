5f25ee172bf1c4d7ffe69563ab830fc726f7b19b
Aliya83311 <hamidullina833@gmail.com>
2022-06-04 04:44:47 +0700

    Add task_4_2

---
+++ Khamidullina_Aliya_dz_4/task_4_2.py
@@ -0,0 +1,28 @@
+import requests
+import xml.etree.ElementTree as et
+from decimal import Decimal, ROUND_HALF_UP
+
+
+def currency_rates(code):
+    url = "http://www.cbr.ru/scripts/XML_daily.asp"
+    response = requests.get(url)
+    result = response.text
+    root = et.fromstring(result)
+    try:
+        code = code.upper()
+    except AttributeError:
+        print("This function takes only strings.")
+    else:
+        for valute in root.findall("Valute"):
+            char_code = valute.find("CharCode").text
+            if char_code == code:
+                value = Decimal(valute.find("Value").text.replace(",", "."))
+                return value.quantize(Decimal("1.00"), ROUND_HALF_UP)
+
+
+print(currency_rates("eur"))
+print(currency_rates("UsD"))
+print(currency_rates(456456456))
+
+
+#     current_date = root.get("Date")
