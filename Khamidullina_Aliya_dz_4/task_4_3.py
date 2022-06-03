db87959b61f69ab14ec6a6196f1905cb563b0efc
Aliya83311 <hamidullina833@gmail.com>
2022-06-04 05:23:49 +0700

    Add task 4_3, fixed minor issues in task_4_2

--- Khamidullina_Aliya_dz_4/task_4_3.py
+++ Khamidullina_Aliya_dz_4/task_4_3.py
@@ -1,4 +1,5 @@
 from decimal import Decimal, ROUND_HALF_UP
+from datetime import datetime
 import requests
 import xml.etree.ElementTree as et
 
@@ -8,6 +9,8 @@ def currency_rates(code):
     response = requests.get(url)
     result = response.text
     root = et.fromstring(result)
+    current_date = root.get("Date")
+    current_date = datetime.strptime(current_date, '%d.%m.%Y').date()
     try:
         code = code.upper()
     except AttributeError:
@@ -15,11 +18,14 @@ def currency_rates(code):
     else:
         for valute in root.findall("Valute"):
             char_code = valute.find("CharCode").text
+            nominal = valute.find("Nominal").text
             if char_code == code:
                 value = Decimal(valute.find("Value").text.replace(",", "."))
-                return value.quantize(Decimal("1.00"), ROUND_HALF_UP)
+                value = value.quantize(Decimal("1.00"), ROUND_HALF_UP)
+                return f"Today {current_date} {nominal} {code} is {value} roubles."
 
 
 print(currency_rates("eur"))
 print(currency_rates("UsD"))
+print(currency_rates("KZT"))
 print(currency_rates(456456456))
