20c856f7d87dd5b20e828eb5e26d3a2f4dc845e8
Aliya83311 <hamidullina833@gmail.com>
2022-06-06 01:45:18 +0700

    Add task_5_1

--- Khamidullina_ALiya_dz_5/task_5_1.py
+++ Khamidullina_ALiya_dz_5/task_5_1.py
@@ -1,21 +1,19 @@
-# basket = ['apple', 'samsung', 'apple', 'huawei', 'asus', 'samsung']
-#
-# unique_brands = set()
-# tmp = set()
-# for el in basket:
-#     if el not in tmp:
-#         unique_brands.add(el)
-#     else:
-#         unique_brands.discard(el)
-#     tmp.add(el)
-# print(unique_brands)
-#
-# unique_brands_ord = [el for el in basket if el in unique_brands]
-# print(unique_brands_ord)
+def odd_nums(end):
+    """ Form an iterable consisting of odd numbers.
 
-a = None
+    :param end: an integer representing the highest value in the iterable
+    :return: the iterable of odd numbers starting from 1 and including end value
+    """
+    try:
+        end = int(end)
+    except ValueError:
+        print("This function takes only integers.")
+    else:
+        for n in range(1, end + 1, 2):
+            yield n
 
-if a:
-    print("yes")
-else:
-    print("nope")
+
+odd_35 = odd_nums(input("Type any number: "))
+print(next(odd_35))
+print(next(odd_35))
+print(next(odd_35))
